9e076d43ad9f5a75a55d41a09effbdb4a4931146
nmatsu2013 <48595242+nmatsu2013@users.noreply.github.com>
2026-02-13 00:00:17 +0900

    usersDto.ts を作成

---
+++ src/interface/dto/usersDto.ts
@@ -0,0 +1,12 @@
+import { z } from "zod"
+
+export const createUserSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+})
+
+export const updateUserSchema = z.object({
+  name: z.string().min(1).optional(),
+  email: z.string().email().optional(),
+  isActive: z.boolean().optional(),
+})
